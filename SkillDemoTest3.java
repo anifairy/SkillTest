27706d4f17c747de62820eb40c85449328ff967f
anifairy <43860686+anifairy@users.noreply.github.com>
2022-05-04 23:23:55 -0700

    updated test

---
+++ SkillDemoTest3.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+
+public class SkillDemoTest3{
+    @Test
+    public void test1() {
+    assertEquals(6,SkillDemo3.multiply(2,3); //failing test 
+    }
+
+}
